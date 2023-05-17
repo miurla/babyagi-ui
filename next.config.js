ee375544dd8f45a8902ab86cc21e949b2831b13f
Cs4K1Sr4C <cs4k1sr4c@tutanota.com>
2023-05-17 04:24:18 +0200

    [#] i18n configuration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
+// @ts-check
+const { i18n } = require('./next-i18next.config');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n,
   reactStrictMode: true,
   webpack(config) {
     config.experiments = {
