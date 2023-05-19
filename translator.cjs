1ec7a4ce510fbd3a53d1e9b8c558ee62355a3e09
Cs4K1Sr4C <cs4k1sr4c@tutanota.com>
2023-05-19 04:40:00 +0200

    Update translator.cjs

--- translator.cjs
+++ translator.cjs
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
-const { Configuration, OpenAIApi } = require('openai');
 
 class Translator {
   constructor(srcLang, targetLang, transService, settings = {}) {
@@ -21,6 +20,7 @@ class Translator {
       this.translatorService === 'openai' &&
       process.env.OPENAI_API_KEY !== ''
     ) {
+    const { Configuration, OpenAIApi } = require('openai');
       this.configuration = new Configuration({
         apiKey: process.env.OPENAI_API_KEY,
       });
@@ -50,8 +50,6 @@ class Translator {
 
   extractKeyNamespacePairs(filePath) {
     let fileContent = fs.readFileSync(filePath, 'utf8');
-    // const regex =
-    //      /(?:translate|i18n\?\.t|t)\(\s*?['"]([^'"]+)['"](?:\s*?,\s*?['"]([^'"]+)['"])(?:\s?,\s*?['"]([^'"]+)['"])?\)/g; // Working
 
     const regex =
       /(?:translate\(|i18n\?\.t\(|t\()['"](?!(?:\\n|a)['"])([^'"]+)['"](?:\s?,\s?['"]([^'"]+)['"])?(?:,\s?['"]([^'"]+)['"])?\)/g;
@@ -128,6 +126,7 @@ class Translator {
         const sourceTranslationValue = this.getTranslationValue(
           srcLangFilePath,
           key,
+          namespace
         );
         if (sourceTranslationValue !== '') {
           const translatedValue = await this.translateText(
@@ -146,7 +145,7 @@ class Translator {
                       this.writeTranslationFile(
                         targetLangFilePath,
                         key,
-                        enteredTranslation,
+                        enteredTranslation
                       );
                       rl.close();
                     },
@@ -234,16 +233,29 @@ class Translator {
     }
   }
 
-  getTranslationValue(filePath, key) {
-    if (!fs.existsSync(filePath)) {
-      return '';
-    }
-    const content = fs.readFileSync(filePath, 'utf8');
-    const translations = JSON.parse(content);
-    return translations[key] || '';
+  
+getTranslationValue(filePath, key, namespace) {
+  if (!fs.existsSync(filePath)) {
+    return '';
   }
+  const content = fs.readFileSync(filePath, 'utf8');
+  const translations = JSON.parse(content);
+  const keys = key.split('.');
+  let value = translations;
+  for (const k of keys) {
+    if (value.hasOwnProperty(k)) {
+      value = value[k];
+    } else {
+      value = '';
+      break;
+    }
+  }
+  return value;
+}
 
-async translateText(sourceTranslationValue) {
+
+
+  async translateText(sourceTranslationValue) {
     if (this.translatorService === 'google') {
       return await this.GoogleTranslate(
         this.srcLang,
@@ -254,8 +266,8 @@ async translateText(sourceTranslationValue) {
       this.translatorService === 'openai' &&
       this.openaiTranslationMethod === 'chat'
     ) {
-      const TRANSLATE_PROMPT = `Translate the ${sourceTranslationValue} text using the ${this.targetLang} language code then respond only with the translated text.`;
-      return await this.translateViaChatCompletion(TRANSLATE_PROMPT);
+      const USER_PROMPT = `What you have to translate is: "${sourceTranslationValue}"\nThe language code you have to use is: ${this.targetLang}`;
+      return await this.translateViaChatCompletion(USER_PROMPT);
     } else if (
       this.translatorService === 'openai' &&
       this.openaiTranslationMethod === 'text'
@@ -298,6 +310,76 @@ async translateText(sourceTranslationValue) {
     }
   }
 
+  async manageFolders() {
+  const allowedLocales = await this.detectAllowedLocales();
+  // Perform folder management operations based on the allowedLocales
+}
+
+async detectAllowedLocales() {
+  // Detect the allowed locales by analyzing the existing translation files
+  // and return an array of detected locales
+  return ["en", "de", "hu", "ja", "ko", "pt"]; // test
+}
+
+async translateProject() {
+  const allowedLocales = await this.detectAllowedLocales();
+  const sourceTranslationDir = path.join(
+    __dirname,
+    'public',
+    'locales',
+    'translation'
+  );
+
+  if (fs.existsSync(sourceTranslationDir)) {
+    const files = fs.readdirSync(sourceTranslationDir);
+    files.forEach(async (file) => {
+      const srcFilePath = path.join(sourceTranslationDir, file);
+      const stats = fs.statSync(srcFilePath);
+
+      if (stats.isFile()) {
+        const content = fs.readFileSync(srcFilePath, 'utf8');
+        const translations = JSON.parse(content);
+
+        for (const locale of allowedLocales) {
+          const targetDir = path.join(
+            __dirname,
+            'public',
+            'locales',
+            locale,
+            'translation'
+          );
+
+          if (!fs.existsSync(targetDir)) {
+            fs.mkdirSync(targetDir, { recursive: true });
+          }
+
+          const targetFilePath = path.join(targetDir, file);
+          const targetContent = await this.translateJSON(translations, locale);
+          fs.writeFileSync(targetFilePath, targetContent, 'utf8');
+        }
+      }
+    });
+  }
+}
+
+async translateJSON(json, locale) {
+  const translatedJson = {};
+
+  await Promise.all(
+    Object.entries(json).map(async ([key, value]) => {
+      if (typeof value === 'object') {
+        translatedJson[key] = await this.translateJSON(value, locale);
+      } else {
+        this.targetLang = locale;
+        const translatedValue = await this.translateText(value);
+        translatedJson[key] = translatedValue;
+      }
+    })
+  );
+
+  return JSON.stringify(translatedJson, null, 2);
+}
+
   // Services
   // GoogleTranslate
   GoogleTranslate = async (
@@ -329,11 +411,13 @@ async translateText(sourceTranslationValue) {
   };
 
   // Translate via OpenAI ChatCompletion
-  translateViaChatCompletion = async (TRANSLATE_PROMPT) => {
+  translateViaChatCompletion = async (USER_PROMPT) => {
+    const MASTER_PROMPT = "You are a professional translator named AT-i18n. You have to wait for the user to provide the translatable text and the target language's code. You have to respond ONLY with the translated text."
     const translationResult = await this.openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
       temperature: 1,
-      messages: [{ role: 'user', content: TRANSLATE_PROMPT }],
+      messages: [ {role: 'system', content: MASTER_PROMPT},
+        { role: 'user', content: USER_PROMPT }],
     });
     return translationResult.data.choices[0].message.content.replace(
       /^['",`]+|['",`]+$/g,
@@ -356,13 +440,15 @@ async translateText(sourceTranslationValue) {
       .split('\n\n')[1]
       .replace(/^['",`]+|['",`]+$/g, '');
   };
-
+  
+  
   // main run
   async run(srcDirectory) {
     this.collectKeyNamespacePairs(srcDirectory);
     await this.translateAndWriteFiles();
     console.log('Translation files generated successfully.');
   }
+
 }
 
 const readline = require('readline');
