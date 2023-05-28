a6bb00f2edfd1829f2c740b92c409014001c515e
Yoshiki Miura <miurap400@gmail.com>
2023-05-29 00:15:06 +0900

    fix icon logic

--- src/components/Sidebar/ExecutionRow.tsx
+++ src/components/Sidebar/ExecutionRow.tsx
@@ -1,9 +1,9 @@
 import { useExecution } from '@/hooks/useExecution';
 import { useExecutionStatus } from '@/hooks/useExecutionStatus';
 import { Execution } from '@/types';
-import { iconFromExecutionType } from '@/utils/execution';
 import { FC } from 'react';
 import { ExtraButton } from './ExtraButton';
+import { AGENT } from '@/utils/constants';
 
 interface ExecutionRowProps {
   execution: Execution;
@@ -23,6 +23,8 @@ export const ExecutionRow: FC<ExecutionRowProps> = ({ execution }) => {
     selectExecution(undefined);
   };
 
+  const agent = AGENT.find((agent) => agent.id === execution.params.agent);
+
   return (
     <button
       className={`flex w-full items-center gap-3 rounded-lg p-3 text-sm transition-colors duration-200 hover:bg-neutral-500/20 disabled:opacity-50 disabled:hover:bg-inherit
@@ -35,9 +37,7 @@ export const ExecutionRow: FC<ExecutionRowProps> = ({ execution }) => {
       onClick={() => handleSelectExecution(execution.id)}
       disabled={isExecuting}
     >
-      <span className="pr-1">
-        {iconFromExecutionType(execution.params.agent)}
-      </span>
+      <span className="pr-1">{agent?.icon}</span>
       <span className="w-full truncate text-left">{execution.name}</span>
       <ExtraButton onDelete={() => deleteHandler(execution.id)} />
     </button>
