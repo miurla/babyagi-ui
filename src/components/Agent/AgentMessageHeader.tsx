1e08d1cd51e3a409ab4ee7bee84b07bf91c3622f
Yoshiki Miura <miurap400@gmail.com>
2023-06-02 06:39:40 +0900

    Update header title

--- src/components/Agent/AgentMessageHeader.tsx
+++ src/components/Agent/AgentMessageHeader.tsx
@@ -4,17 +4,17 @@ import { translate } from '../../utils/translate';
 
 interface AgentMessageHeaderProps {
   model: SelectItem;
-  iterations: SelectItem;
+  agent: SelectItem;
 }
 
 export const AgentMessageHeader: FC<AgentMessageHeaderProps> = ({
   model,
-  iterations,
+  agent,
 }) => {
   return (
     <div className="flex justify-center border border-b-neutral-300 bg-neutral-100 py-2 text-sm text-neutral-500 dark:border-none dark:bg-[#444654] dark:text-neutral-200">
-      {`${translate('MODEL')}: ${model.name} | ${translate('ITERATION')}: ${
-        iterations.name
+      {`${translate('MODEL')}: ${model.name} | ${translate('AGENT')}: ${
+        agent.name
       }`}
     </div>
   );
